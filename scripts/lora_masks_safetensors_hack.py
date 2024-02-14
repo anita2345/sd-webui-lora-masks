8aa77fff164715ad420a75381965d791b2e79aa3
anita2345 <75013412+anita2345@users.noreply.github.com>
2024-02-14 18:39:57 +0000

    attempt fix safetensor load

--- scripts/lora_masks_safetensors_hack.py
+++ scripts/lora_masks_safetensors_hack.py
@@ -4,7 +4,7 @@
 # I just added support to allow more masks for the LoRA models
 
 import os
-import mmap
+#import mmap
 import torch
 import json
 import safetensors.torch
@@ -18,16 +18,14 @@ UntypedStorage = torch.storage.UntypedStorage if hasattr(
 def load_file(filename, device):
     """"Loads a .safetensors file without memory mapping that locks the model file.
     Works around safetensors issue: https://github.com/huggingface/safetensors/issues/164"""
-    with open(filename, mode="r", encoding="utf8") as file_obj:
-        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as m:
-            header = m.read(8)
-            n = int.from_bytes(header, "little")
-            metadata_bytes = m.read(n)
-            metadata = json.loads(metadata_bytes)
+    with open(filename, mode="rb") as file_obj:
+        buffer = file_obj.read()
+        metadata_size = int.from_bytes(buffer[:8], "little")
+        metadata_bytes = buffer[8:8 + metadata_size]
+        metadata = json.loads(metadata_bytes.decode('utf-8'))
 
-    size = os.stat(filename).st_size
-    storage = UntypedStorage.from_file(filename, False, size)
-    offset = n + 8
+    storage = torch.ByteStorage.from_buffer(buffer[metadata_size + 8:])
+    offset = metadata_size + 8
     md = metadata.get("__metadata__", {})
     return {name: create_tensor(storage, info, offset) for name, info in metadata.items() if name != "__metadata__"}, md
 
@@ -38,11 +36,8 @@ DTYPES = {
     "F16": torch.float16,
     "BF16": torch.bfloat16,
     "I64": torch.int64,
-    # "U64": torch.uint64,
     "I32": torch.int32,
-    # "U32": torch.uint32,
     "I16": torch.int16,
-    # "U16": torch.uint16,
     "I8": torch.int8,
     "U8": torch.uint8,
     "BOOL": torch.bool
@@ -55,4 +50,4 @@ def create_tensor(storage, info, offset):
     dtype = DTYPES[info["dtype"]]
     shape = info["shape"]
     start, stop = info["data_offsets"]
-    return torch.asarray(storage[start + offset: stop + offset], dtype=torch.uint8).view(dtype=dtype).reshape(shape).clone().detach()
+    return torch.tensor(storage[start + offset: stop + offset], dtype=dtype, device='cpu').view(shape).clone().detach()
